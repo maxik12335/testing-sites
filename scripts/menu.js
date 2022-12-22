1bc42605e41e590dd1daf336c36951d5cecd76d2
Maksim Kuznetsov <m.kuznecov@krediska.ru>
2022-12-22 08:35:55 +0300

    testign designer

---
+++ scripts/menu.js
@@ -0,0 +1,51 @@
+let count = 0
+const menu = document.querySelector(".header")
+const burger = document.querySelector(".burger")
+const burgerItems = document.querySelectorAll(".burger__item")
+
+burger.addEventListener("click", () => {
+  if(menu.classList.contains("header-show")) {
+    menu.classList.remove("header-show")
+
+    burger.classList.remove("burger-close")
+
+    burgerItems[0].style.transform = "rotate(0deg)"
+    burgerItems[0].style.position = "relative"
+    burgerItems[1].style.transform = "rotate(0deg)"
+    burgerItems[1].style.position = "relative"
+    burgerItems[2].style.display = "block"
+  } else {
+    menu.classList.add("header-show")
+    
+    burger.classList.add("burger-close")
+
+    burgerItems[0].style.transform = "rotate(45deg)"
+    burgerItems[0].style.position = "absolute"
+    burgerItems[1].style.transform = "rotate(-45deg)"
+    burgerItems[1].style.position = "absolute"
+    burgerItems[2].style.display = "none"
+
+  }
+  
+})
+
+window.addEventListener("scroll", () => {
+  if(count > pageYOffset) {
+    console.log(count + " НАВЕРХ")
+    menu.classList.remove("header-hide")
+    burger.classList.remove("burger-hide")
+  }
+
+  if(count < pageYOffset) {
+    console.log(count + " ВНИЗ")
+    menu.classList.add("header-hide")
+    burger.classList.add("burger-hide")
+  }
+
+  if(document.querySelector(".movie-modal-open")) {
+    menu.classList.add("header-hide")
+    burger.classList.add("burger-hide")
+  }
+
+  count = pageYOffset
+})
---
+++ scripts/product.js
@@ -0,0 +1,31 @@
+const product = document.querySelectorAll(".product__item")
+const productTitles = document.querySelectorAll(".product__title")
+const productPlays = document.querySelectorAll(".product__play")
+const movieModal = document.querySelector(".movie-modal")
+const video = document.querySelector(".video")
+const closeModal = document.querySelector(".modal-close")
+
+product.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    const movie = item.getAttribute("data-value")
+    video.setAttribute("src", `movies/${movie}.mp4`)
+    movieModal.classList.add("movie-modal-open");
+
+    document.querySelector(".header").classList.add("header-hide")
+  })
+
+  item.addEventListener("mouseover", () => {
+    productTitles[index].classList.add("product__title-hide")
+    productPlays[index].classList.add("product__play-show")
+  })
+
+  item.addEventListener("mouseout", () => {
+    productTitles[index].classList.remove("product__title-hide")
+    productPlays[index].classList.remove("product__play-show")
+  })
+})
+
+closeModal.addEventListener("click", () => {
+  movieModal.classList.remove("movie-modal-open")
+})
+
